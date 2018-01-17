d5f19ea82cf7ded6860566bc6f924466316d75cd
James Smith <smi.james.th@gmail.com>
2018-01-17 16:45:34 +0200

    Eventually got around to changing the default filesize to zero.

--- Main.cpp
+++ Main.cpp
@@ -76,7 +76,7 @@ int main(int iArgC, char *pchaArgV[])
             ("roach-tcpborph-port,w", boost::program_options::value<uint16_t>(&u16ROACHTCPBorphPort)->default_value(7147), "Port to use for connection to ROACH TCPBorph server.")
             ("roach-gateware-dir,g", boost::program_options::value<string>(&strRoachGatewareDirectory)->default_value(string("/home/avnuser/RoachLaunchers")), "Directory containing Roach launcher script and FPG file s.")
             ("recording-dir,p", boost::program_options::value<string>(&strRecordingDir)->default_value(string("/home/avnuser/Data/RoachAcquisition")), "Path to directory where HDF5 will be recorded.")
-            ("max-file-size,q", boost::program_options::value<uint32_t>(&u32MaxFileSize_MB)->default_value(1024), "Maximum HDF5 file size in MB. A new files will be created after this limit is reached (give or take metadata size). 0 implies no limit.");
+            ("max-file-size,q", boost::program_options::value<uint32_t>(&u32MaxFileSize_MB)->default_value(0), "Maximum HDF5 file size in MB. A new files will be created after this limit is reached (give or take metadata size). 0 implies no limit.");
 
 
     boost::program_options::variables_map oVariableMap;
