b54a35d467618efa6fe51060e1fb58c96a942af8
James Smith <smi.james.th@gmail.com>
2018-01-30 15:07:53 +0200

    Forgot to add KATCPServer files.

---
+++ SCTestBed/KATCPServer.cpp
@@ -0,0 +1,114 @@
+// System includes
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <ctime>
+
+// Library includes
+extern "C" {
+  #include <katpriv.h>
+}
+
+#include <boost/thread/thread.hpp>
+
+
+// Local includes
+#include "KATCPServer.h"
+#include "../AVNUtilLibs/Sockets/InterruptibleBlockingSockets/InterruptibleBlockingTCPSocket.h"
+
+using namespace std;
+
+//Extract function for boolean sensors which don't care whether or not they're true or false.
+//Courtesy Marc Welz
+int extract_dontcare_boolean_katcp(struct katcp_dispatch *d, struct katcp_sensor *sn)
+{
+  set_status_sensor_katcp(sn, KATCP_STATUS_NOMINAL);
+  return 0;
+}
+
+// Define static members of cKATCPServer
+double                            cKATCPServer::m_dSkyActualAzim_deg;
+boost::scoped_ptr<boost::thread>  cKATCPServer::m_pKATCPThread;
+struct katcp_dispatch             *cKATCPServer::m_pKATCPDispatch;
+std::string                                         cKATCPServer::m_strListenInterface;
+uint16_t                                            cKATCPServer::m_u16Port;
+uint32_t                                            cKATCPServer::m_u32MaxClients;
+
+cKATCPServer::cKATCPServer(const string &strListenInterface, uint16_t u16Port, uint32_t u32MaxClients)
+{
+  cout << "Starting Station Controller virtualiser..." << endl;
+  startServer(strListenInterface, u16Port, u32MaxClients);
+}
+
+cKATCPServer::~cKATCPServer()
+{
+  cout << "Stopping Station Controller virtualiser..." << endl;
+  stopServer();
+  cout << "Station Controller virtualiser stopped." << endl;
+}
+
+void cKATCPServer::startServer(const std::string &strListenInterface, uint16_t u16Port, uint32_t u32MaxClients)
+{
+  cout << "Starting KATCP server." << endl;
+
+  //Store config parameters in members
+  m_strListenInterface    = strListenInterface;
+  m_u16Port               = u16Port;
+  m_u32MaxClients         = u32MaxClients;
+
+  m_pKATCPThread.reset(new boost::thread(&cKATCPServer::serverThreadFunction));
+}
+
+void cKATCPServer::stopServer()
+{
+  cout << "Stopping KATCP server." << endl;
+
+  terminate_katcp(m_pKATCPDispatch, KATCP_EXIT_QUIT); //Stops server
+
+  //Make socket connection to KATCP server to force its main loop to exit
+  {
+      cout << "Connect temporary socket to KATCP server to force it to evaluate shutdown request..." << endl;
+      cInterruptibleBlockingTCPSocket oTempSocket(m_strListenInterface, m_u16Port);
+  }
+
+  //Now we can join the server thread
+  m_pKATCPThread->join();
+  m_pKATCPThread.reset();
+
+  shutdown_katcp(m_pKATCPDispatch); //Cleans up memory of the dispatch struct
+
+}
+
+void cKATCPServer::serverThreadFunction()
+{
+  cout << "Server thread function." << endl;
+  m_pKATCPDispatch = startup_katcp();
+  if(!m_pKATCPDispatch)
+  {
+      cout << "Error allocating KATCP server state." << endl;
+  }
+
+  /* load up build and version information */
+  //Get compile time
+  stringstream oSSCompileTime;
+  oSSCompileTime << string(__DATE__);
+  oSSCompileTime << string(" ");
+  oSSCompileTime << string(__TIME__);
+
+  //Add a version number to KATCTP server
+  add_version_katcp(m_pKATCPDispatch, const_cast<char*>("StationControllerSimulator"), 0, const_cast<char*>("0.9"), &oSSCompileTime.str()[0]);
+
+  //Make a server listening interface from hostname and port string
+  stringstream oSSServer;
+  oSSServer << m_strListenInterface;
+  oSSServer << string(":");
+  oSSServer << m_u16Port;
+
+  if(run_multi_server_katcp(m_pKATCPDispatch, m_u32MaxClients, &oSSServer.str()[0], 0) < 0)
+  {
+      cout << "Error starting KATCP server." << endl;
+  }
+
+  cout << "Exiting server thread." << endl;
+
+}
---
+++ SCTestBed/KATCPServer.h
@@ -0,0 +1,44 @@
+#ifndef KATCTP_SERVER_H
+#define KATCTP_SERVER_H
+
+// System includes
+#include <string>
+#include <vector>
+#include <inttypes.h>
+
+//Library includes
+extern "C" {
+#include <katcp.h>
+}
+
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/thread.hpp>
+
+class cKATCPServer
+{
+public:
+  cKATCPServer(const std::string &strListenInterface = std::string("0.0.0.0"), uint16_t u16Port = 40001, uint32_t u32MaxClients = 5);
+  ~cKATCPServer();
+
+  static void startServer(const std::string &strListenInterface, uint16_t u16Port, uint32_t u32MaxClients);
+  static void stopServer();
+  static void serverThreadFunction();
+
+protected:
+  static double    getSkyActualAzim(struct katcp_dispatch *pD, struct katcp_acquire *pA);
+
+private:
+
+  static std::string                                      m_strListenInterface;
+  static uint16_t                                         m_u16Port;
+  static uint32_t                                         m_u32MaxClients;
+  
+  static double                           m_dSkyActualAzim_deg;
+  static boost::scoped_ptr<boost::thread> m_pKATCPThread;
+  static struct katcp_dispatch            *m_pKATCPDispatch;
+
+
+};
+
+#endif //KATCTP_SERVER_H
