b297a493a5b2fc128ce1a4a53f3b13c5addced04
James Smith <smi.james.th@gmail.com>
2018-08-30 15:31:11 +0200

    Apparently it reads correctly but not writes. Trying to rectify.

--- KATCPServer.cpp
+++ KATCPServer.cpp
@@ -857,7 +857,7 @@ int32_t cKATCPServer::roachSetDspGain_KATCPCallback(struct katcp_dispatch *pKATC
     }
 
     // digital_gain register has a binary-point of 12 bits
-    double dRequestedGain = strtod(arg_string_katcp(pKATCPDispatch, 1), NULL) / pow(2,12);
+    double dRequestedGain = strtod(arg_string_katcp(pKATCPDispatch, 1), NULL);// / pow(2,12);
 
     if (dRequestedGain < 0)
     {
